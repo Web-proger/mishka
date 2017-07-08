2a0773c4e621006416c642f9d4f5061b149f21c2
Иван Тарасов <guser999@mail.ru>
2017-07-08 19:31:50 +0700

    Сделал кнопку меню, а также оживил её в JS

---
+++ js/script.js
@@ -0,0 +1,16 @@
+var mainMenu = document.querySelector(".main-menu");
+var menuBtn = document.querySelector(".page-header__menu-btn");
+
+mainMenu.classList.remove("main-menu--no-js");
+menuBtn.classList.remove("page-header__menu-btn--no-js");
+
+menuBtn.addEventListener("click", function(event){
+  event.preventDefault();
+  if (menuBtn.classList.contains("page-header__menu-btn--closed")) {
+    menuBtn.classList.remove("page-header__menu-btn--closed");
+    menuBtn.classList.add("page-header__menu-btn--opened");
+  } else {
+    menuBtn.classList.remove("page-header__menu-btn--opened");
+    menuBtn.classList.add("page-header__menu-btn--closed");
+  }
+});
